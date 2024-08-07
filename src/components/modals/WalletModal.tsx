30d2fb97e8445da06d39612763891d3122326823
timewn1 <timewornbn@gmail.com>
2024-08-07 09:58:58 +0800

    feat: update ui

--- src/components/modals/WalletModal.tsx
+++ src/components/modals/WalletModal.tsx
@@ -5,7 +5,7 @@ export function WalletModal({close}: any) {
   const { connectors, connect } = useConnect();
 
   return (
-    <div className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black/20">
+    <div className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen max-h-full bg-black/20">
       <div className="absolute p-4 w-full max-w-md max-h-full top-[15%] left-[50%] -translate-x-[50%]">
         <div className="relative bg-white rounded-lg shadow">
           <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
